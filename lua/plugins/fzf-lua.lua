bfa64b98218ff2d2071d1ea39d914207ecce6cb6
Vadim <rudometov.vadim123@gmail.com>
2025-03-23 01:29:47 +0300

    Add treesitter

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -3,6 +3,6 @@ return {
   -- optional for icon support
   -- dependencies = { "nvim-tree/nvim-web-devicons" },
   -- or if using mini.icons/mini.nvim
-  -- dependencies = { "echasnovski/mini.icons" },
+  dependencies = { "echasnovski/mini.icons" },
   opts = {}
 }
