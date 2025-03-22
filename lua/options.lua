0991eb6112da86a9bd61cb3c416ac155e186f749
Vadim <rudometov.vadim123@gmail.com>
2025-03-22 23:40:23 +0300

    Options and lazy plug-in manager

---
+++ lua/options.lua
@@ -0,0 +1,27 @@
+local options = {
+	-- line numbers
+	number = true,
+	relativenumber = true,
+	signcolumn = 'yes',
+
+	-- split settings
+	splitbelow = true,
+	splitright = true,
+
+	-- persistent undo
+	undofile = true,
+	
+	showcmd = true,
+
+	-- tab thingies
+	tabstop = 4,
+	shiftwidth = 4,
+	expandtab = true,
+	
+	ignorecase = true,
+	laststatus = 3,
+}
+
+for n, v in pairs(options) do
+	vim.opt[n] = v
+end
---
+++ lua/plugins/vscode-colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"Mofiqul/vscode.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require('vscode').setup({
+				transparent = true,
+				italic_comments = true,
+				disable_nvimtree_ng = true,
+				terminal_colors = true,
+			})
+			vim.cmd([[colorscheme vscode]])
+		end,
+	},
+}
