a068dd026b8111c76ee2f7e1d29aacf168a4e22c
Vadim <rudometov.vadim123@gmail.com>
2025-03-24 11:05:26 +0300

    Remove ugly highlight for markdown headings

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -7,5 +7,11 @@ return {
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
-    opts = {},
+    opts = {
+        heading = {
+            backgrounds = {
+                
+            },
+        },
+    },
 }
