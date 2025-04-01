50e9014e91f1b0f99e4eb5d98edd0f12677b275b
Vadim <rudometov.vadim123@gmail.com>
2025-04-01 11:10:39 +0300

    Add lsp and cmp for go

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,59 @@
+local function config(_config)
+    return vim.tbl_deep_extend('force', {
+        capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    }, _config or {})
+end
+
+function setup_gopls()
+    util = require('lspconfig.util')
+    require('lspconfig').gopls.setup({
+        on_attach = on_attach,
+        cmd = { 'gopls', },
+        filetypes = { 'go', 'go.mod' },
+        root_dir = util.root_pattern('go.work', 'go.mod', '.git'),
+        settings = {
+            gopls = {
+                analyses = {
+                    unusedparams = true,
+                    shadow = true,
+                },
+                staticcheck = true,
+            }
+        }
+    })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+        desc = 'Automatic Golang formatting',
+        pattern = "*.go",
+        callback = function()
+            local params = vim.lsp.util.make_range_params()
+            params.context = {only = {"source.organizeImports"}}
+            -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+            -- machine and codebase, you may want longer. Add an additional
+            -- argument after params if you find that you have to write the file
+            -- twice for changes to be saved.
+            -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+            local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+            for cid, res in pairs(result or {}) do
+                for _, r in pairs(res.result or {}) do
+                    if r.edit then
+                        local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                        vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                    end
+                end
+            end
+            vim.lsp.buf.format({async = false})
+        end
+    })
+end
+
+function setup_lsps()
+    setup_gopls()
+end
+
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "olexsmir/gopher.nvim",
+    },
+    config = setup_lsps,
+}
---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,42 @@
+return {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+        'neovim/nvim-lspconfig',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-cmdline',
+        'hrsh7th/cmp-vsnip',
+        'hrsh7th/vim-vsnip',
+    },
+    config = function()
+        cmp = require('cmp')
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+                    -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+                    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+                    -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+
+                    -- For `mini.snippets` users:
+                    -- local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
+                    -- insert({ body = args.body }) -- Insert at cursor
+                    -- cmp.resubscribe({ "TextChangedI", "TextChangedP" })
+                    -- require("cmp.config").set_onetime({ sources = {} })
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+            }),
+        })
+    end,
+}
