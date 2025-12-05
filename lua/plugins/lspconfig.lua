e3cc389cb5d03d6b78de5fb6298292d7ced83582
Vadim <rudometov.vadim123@gmail.com>
2025-12-05 21:10:41 +0300

    fix: lua_ls configuration

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -229,38 +229,21 @@ function SetupLsps()
             -- Else, DO load Neovim's libraries
 
             ---@diagnostic disable-next-line: param-type-mismatch
-            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            client.config.settings.Lua = {
                 runtime = {
-                    -- Tell the language server which version of Lua you're using (most
-                    -- likely LuaJIT in the case of Neovim)
                     version = 'LuaJIT',
-                    -- Tell the language server how to find Lua modules same way as Neovim
-                    -- (see `:h lua-module-load`)
                     path = {
                         'lua/?.lua',
                         'lua/?/init.lua',
                         'lua/plugins/?.lua'
                     },
                 },
-                -- Make the server aware of Neovim runtime files
                 workspace = {
                     checkThirdParty = false,
-                    library = {
-                        vim.env.VIMRUNTIME,
-                        -- Depending on the usage, you might want to add additional paths
-                        -- here.
-                        -- '${3rd}/luv/library'
-                        -- '${3rd}/busted/library'
-                    }
-                    -- Or pull in all of 'runtimepath'.
-                    -- NOTE: this is a lot slower and will cause issues when working on
-                    -- your own configuration.
-                    -- See https://github.com/neovim/nvim-lspconfig/issues/3189
-                    -- library = {
-                    --   vim.api.nvim_get_runtime_file('', true),
-                    -- }
-                }
-            })
+                    library = { vim.env.VIMRUNTIME },
+                },
+                telemetry = false,
+            }
         end,
         settings = {
             Lua = {}
